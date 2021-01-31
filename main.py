4888471e8af3ae14a4682413ff5f46dad87df351
sumityadav1509 <sumityadav2005@gmail.com>
2021-01-31 11:00:37 +0000

    Debugging using print function.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 #   for i in range(1,21):
 #     if i == 20:
 #       print("You got it")
-# my_function()
+# my_function() 
 
 # # Reproduce the Bug
 # from random import randint
@@ -14,24 +14,24 @@
 # print(dice_imgs[dice_num])
 
 # # Play Computer
-year = int(input("What's your year of birth?"))
-if year >= 1980 and year < 1994:
-  print("You are a millenial.")
-elif year >= 1994:
-  print("You are a Gen Z.")
+# year = int(input("What's your year of birth?"))
+# if year >= 1980 and year < 1994:
+#   print("You are a millenial.")
+# elif year >= 1994:
+#   print("You are a Gen Z.")
 
 # # Fix the Errors
-# age = input("How old are you?")
-# if age > 18:
-# print("You can drive at age {age}.")
+# age = int(input("How old are you? "))
+# if age > int(18):
+#  print(f"You can drive at age {age}.")
 
 # #Print is Your Friend
-# pages = 0
-# word_per_page = 0
-# pages = int(input("Number of pages: "))
-# word_per_page == int(input("Number of words per page: "))
-# total_words = pages * word_per_page
-# print(total_words)
+pages = 0
+word_per_page = 0
+pages = int(input("Number of pages: "))
+word_per_page = int(input("Number of words per page: "))
+total_words = pages * word_per_page
+print(total_words)
 
 # #Use a Debugger
 # def mutate(a_list):
