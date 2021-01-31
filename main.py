890b0ab52e769709c77100d0435b484533b470dd
sumityadav1509 <sumityadav2005@gmail.com>
2021-01-31 13:59:02 +0000

    Odd-Even code debugging completed.

--- main.py
+++ main.py
@@ -1,45 +1,16 @@
-############DEBUGGING#####################
 
-# # Describe Problem
-# def my_function(): 
-#   for i in range(1,21):
-#     if i == 20:
-#       print("You got it")
-# my_function() 
+# With an error: 
 
-# # Reproduce the Bug
-# from random import randint
-# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
-# dice_num = randint(0,5) 
-# print(dice_imgs[dice_num])
+number=int(input("Type a number to check for even or odd. ")) 
+if number%2=0:
+  print("The number is even. ")
+else:
+  print("the number is odd. ")  
 
-# # Play Computer
-# year = int(input("What's your year of birth?"))
-# if year >= 1980 and year < 1994:
-#   print("You are a millenial.")
-# elif year >= 1994:
-#   print("You are a Gen Z.")
+# Debugging Completed: 
+number=int(input("Type a number to check for even or odd. ")) 
+if number%2==0:
+  print("The number is even. ")
+else:
+  print("the number is odd. ")  
 
-# # Fix the Errors
-# age = int(input("How old are you? "))
-# if age > int(18):
-#  print(f"You can drive at age {age}.")
-
-# #Print is Your Friend
-# pages = 0
-# word_per_page = 0
-# pages = int(input("Number of pages: "))
-# word_per_page = int(input("Number of words per page: "))
-# total_words = pages * word_per_page
-
-# print(total_words)
-
-# #Use a Debugger
-def mutate(a_list):
-  b_list = []
-  for item in a_list:
-    new_item = item * 2
-    b_list.append(new_item)
-  print(b_list)
-
-mutate([1,2,3,5,8,13])
